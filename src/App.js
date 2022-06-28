0a72c2364104e191f2a40bd1b5be622ed4920f90
MahmoudBadawy4199 <m.badawy2011@gmail.com>
2022-06-28 15:10:49 +0200

    counterAdded

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import './App.css';
+import Counter from './components/counter';
 
 function App() {
- 
+ return (
+
+  <>
+  <Counter></Counter>
+  </>
+
+ )
+
 }
 
 export default App;
---
+++ src/components/counter.js
@@ -0,0 +1,29 @@
+import React from "react";
+import {useState} from 'react';
+
+function Counter() {
+    const [counter, setCounter] = useState(0);
+    const inc = () => {
+        setCounter(counter + 1);
+    }
+
+    const dec = () => {
+        if (counter !== 0) {
+            setCounter(counter - 1);
+        }
+    }
+    return (
+        <div className="counter__container">
+            <p className='counter__container__counter'>{counter}</p>
+            <button className='counter__container__dec-btn' onClick={dec}>-</button>
+            <button className='counter__container__inc-btn' onClick={inc}>+</button>
+
+        </div>
+
+
+    );
+
+}
+
+
+export default Counter;
