f0eaa3e09464fe36475ef6eba8504de97cbc0a1c
MahmoudBadawy4199 <m.badawy2011@gmail.com>
2022-07-03 22:34:27 +0200

    add-font

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { store } from './redux'
+import '../src/assets/fonts/montserrat/Montserrat-VariableFont_wght.ttf';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/pages/home/home.page.jsx
+++ src/pages/home/home.page.jsx
@@ -1,8 +1,9 @@
+import './home.page.css'
 function HomePage() {
     return (
-    <h2 className="text-center container">
+    <p className="text-center container">
    Home page
-    </h2>
+    </p>
     );
     }
     export default HomePage;
